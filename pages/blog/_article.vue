e90031df56091344dc1c824a2338f5a3413d08a9
seth <seth@sethpainter.com>
2021-06-09 08:55:50 +0000

    Started adding Table of Contents and article aside

--- pages/blog/_article.vue
+++ pages/blog/_article.vue
@@ -1,11 +1,29 @@
 <template>
-	<div class="container">
+	<main class="container">
+		<aside class="base-border">
+			<div id="categories">
+				<h2>Categories</h2>
+				<hr>
+				<ul>
+					<li v-if="typeof article.categories == 'string'" v-text="article.categories" />
+					<li v-else v-for="category in article.categories" :key="category" v-text="category" />
+				</ul>
+			</div>
+			<h2>On This Page</h2>
+			<hr>
+			<ul id="contents">
+				<li v-for="link of article.toc" :key="link.id" :class="{ 'toc2': link.depth === 2, 'toc3': link.depth === 3 }">
+					<NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+				</li>
+			</ul>
+		</aside>
 		<article class="panel page">
 			<nav-back text="Blog" path="./" />
 			<h1 class="title" v-text="article.title" />
+			<span id="date-written">Written: {{new Date(article.createdAt)}}, Updated: {{article.updatedAt}}</span>
 			<nuxt-content :document="article" />
 		</article>
-	</div>
+	</main>
 </template>
 
 <script>
@@ -19,16 +37,49 @@ export default {
 </script>
 
 <style scoped>
-	.container {
-		display: block;
+	main {
+		display: flex;
+		align-items: flex-start;
+		justify-content: center;
+		margin: var(--margin-buffer-desktop) auto;
+		width: 100%;
+	}
+	aside {
+		background: var(--main-bkg);
+		max-width: 300px;
+		padding: 1em;
+	}
+	#categories ul {
+		list-style-type: none;
+		padding: 1em 0.5em;
+	}
+	#categories li {
+		background: var(--body-bkg);
+		display: inline-block;
+		padding: 0.1em 0.4em;
+		margin: 0.25em;
+	}
+	#categories li:hover {
+		background: darkgrey;
+	}
+	ul#contents {
+		list-style-position: inside;
+	}
+	#contents li {
+		margin: 1em;
+	}
+	#contents a {
+		color: inherit;
 	}
 	h1, h2 {
 		font-family: 'Zilla Slab';
 	}
+	article {
+		text-align: left;
+		margin: 0 1em;
+		max-width: 1200px;
+	}
 	.title {
 		font-size: 2em;
-	}
-	.panel {
-		text-align: left;
 	}
 </style>
