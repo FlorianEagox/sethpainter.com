fd99880d9c79df26bc742cb8ef11a62ee4749f35
seth <seth@sethpainter.com>
2021-06-16 22:09:12 +0000

    Added custom 404 page & NGINX config

---
+++ pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+	<main class="panel base-border">
+		<h1>404 - The requested page or resource couldn't be found</h1>
+		<hr>
+		<p>Perhaps it has been moved or you have entered the wrong link</p>
+		<p><nuxt-link to="/">Click here to go back home</nuxt-link></p>
+	</main>
+</template>
+
+<style scoped>
+	p {
+		margin: 1em;
+	}
+	a {
+		color: var(--accent-color)
+	}
+</style>
